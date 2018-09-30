44a3145f56c9e22c6e829e0d137e6dee1b3852e1
Josh Weinmann <weinmanj@mail.gvsu.edu>
2018-09-30 12:30:09 -0400

    Changed Game.java
    Added comments and removed status bar for now

--- src/tetris/Game.java
+++ src/tetris/Game.java
@@ -1,20 +1,17 @@
 package tetris;
 
 import java.awt.BorderLayout;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
 public class Game extends JFrame {
 
-    JLabel statusbar;
+    /*
+    Default constructor to create game
+     */
+    public Game() {
 
-
-    public Tetris() {
-
-        statusbar = new JLabel(" 0");
-        add(statusbar, BorderLayout.SOUTH);
+        // create board
         Board board = new Board(this);
         add(board);
         board.start();
@@ -24,14 +21,12 @@ public class Game extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    public JLabel getStatusBar() {
-        return statusbar;
-    }
-
+    /*
+    Main method to run game
+     */
     public static void main(String[] args) {
         Game game = new Game();
         game.setLocationRelativeTo(null);
         game.setVisible(true);
-
     }
 }
